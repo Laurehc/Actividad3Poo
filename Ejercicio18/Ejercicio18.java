db70a3cdc104ca7409a5810a046a45e72e50e25f
Laurehc <111622067+Laurehc@users.noreply.github.com>
2022-10-01 17:49:44 -0500

    Update and rename Ejercicio18.txt to Ejercicio18.java

---
+++ Ejercicio18/Ejercicio18.java
@@ -0,0 +1,11 @@
+package ejercicio18;
+
+public class Ejercicio18 {
+
+
+    public static void main(String[] args) {
+        ventana frm = new ventana();
+        frm.setVisible(true);
+    }
+    
+}
