d78513cd5b5e77df2d71d0453c55b4e2ce031867
Laurehc <111622067+Laurehc@users.noreply.github.com>
2022-10-01 19:35:49 -0500

    Create Ejercicio19.java

---
+++ Ejercicio19/Ejercicio19.java
@@ -0,0 +1,10 @@
+package ejercicio19;
+
+public class Ejercicio19 {
+
+    
+    public static void main(String[] args) {
+        Formulario frm = new Formulario();
+        frm.setVisible(true);
+    } 
+}
