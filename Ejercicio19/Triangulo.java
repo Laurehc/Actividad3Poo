e3e4ca7125463af52151135646504b2ef72d2021
Laurehc <111622067+Laurehc@users.noreply.github.com>
2022-10-01 19:36:42 -0500

    Add files via upload

---
+++ Ejercicio19/Triangulo.java
@@ -0,0 +1,23 @@
+package ejercicio19;
+
+public class Triangulo {
+    public static double calcular_perimetro(double lado){
+        double perimetro;
+        perimetro = lado * 3;
+        return perimetro;
+    }
+    
+    public static double calcular_altura(double lado){
+        double altura;
+        altura = (lado * Math.sqrt(3))/2;
+        return altura;
+    }
+    
+    public static double calcular_area(double lado){
+        double area;
+        area = (Math.pow(lado,2) * Math.sqrt(3))/4;
+        return area;
+    }
+    
+    
+}
