33153cd2c53f9aff2505f69c7d1096bf6c994971
Laurehc <111622067+Laurehc@users.noreply.github.com>
2022-10-02 14:12:57 -0500

    Create Ejercicio22.java

---
+++ Ejercicio22/Ejercicio22.java
@@ -0,0 +1,11 @@
+package ejercicio22;
+
+public class Ejercicio22 {
+
+    public static void main(String[] args) {
+         salario frm = new salario();
+         frm.setVisible(true);
+       
+    }
+    
+}
