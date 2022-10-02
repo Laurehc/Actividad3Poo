db779235f2331291460fd33f21858949278b7305
Laurehc <111622067+Laurehc@users.noreply.github.com>
2022-10-02 14:42:39 -0500

    Create Ejercicio40.java

---
+++ Ejercicio40/Ejercicio40.java
@@ -0,0 +1,11 @@
+package ejercicio40;
+
+public class Ejercicio40 {
+
+   
+    public static void main(String[] args) {
+         ventana frm = new ventana();
+         frm.setVisible(true);
+    }
+    
+}
