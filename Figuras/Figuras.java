a3a3be38cb9a979a1987e955830fa00ae10e914e
Laurehc <111622067+Laurehc@users.noreply.github.com>
2022-10-02 18:54:03 -0500

    Create Figuras.java

---
+++ Figuras/Figuras.java
@@ -0,0 +1,12 @@
+package figuras;
+
+public class Figuras {
+
+ 
+    public static void main(String[] args) {
+         ventana frm = new ventana();
+         frm.setVisible(true);
+
+    }
+    
+}
