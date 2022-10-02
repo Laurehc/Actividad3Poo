b5bc1f6b153dab061bf44f999c8422428c99dd27
Laurehc <111622067+Laurehc@users.noreply.github.com>
2022-10-02 13:52:01 -0500

    Create Ejercicio41.java

---
+++ Ejercicio41/Ejercicio41.java
@@ -0,0 +1,11 @@
+package ejercicio41;
+
+public class Ejercicio41 {
+
+    public static void main(String[] args) {
+        calculo frm = new calculo();
+        frm.setVisible(true);
+       
+    }
+    
+}
