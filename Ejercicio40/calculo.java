6f731606be3e20bcdb4c274f79e89aaded2da561
Laurehc <111622067+Laurehc@users.noreply.github.com>
2022-10-02 14:43:01 -0500

    Add files via upload

---
+++ Ejercicio40/calculo.java
@@ -0,0 +1,14 @@
+
+package ejercicio40;
+
+public class calculo {
+    public static double cuadrados(double x){
+        return Math.pow(x,2);
+    }
+    public static double cubos(double x){
+        return Math.pow(x,3);
+    }
+    public static double raices(double x){
+        return Math.sqrt(x);
+    }
+}
